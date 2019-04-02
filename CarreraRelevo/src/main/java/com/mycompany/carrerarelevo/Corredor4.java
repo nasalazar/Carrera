024d6c359bb1bd7f992340eda087f8dbe844f260
nasalazar <38019607+nasalazar@users.noreply.github.com>
2019-04-02 08:55:45 -0500

    Proyecto carrera

---
+++ CarreraRelevo/src/main/java/com/mycompany/carrerarelevo/Corredor4.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.carrerarelevo;
+
+/**
+ *
+ * @author NATHALY
+ */
+public class Corredor4 extends Thread{
+    @Override
+    public void run(){
+        
+    }
+}
---
+++ CarreraRelevo/src/main/java/com/mycompany/carrerarelevo/Corredor9.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.carrerarelevo;
+
+/**
+ *
+ * @author NATHALY
+ */
+public class Corredor9 extends Thread {
+    
+    @Override
+    public void run(){
+        
+    }
+}
